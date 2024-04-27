6fe312900b74d9fde69c5834199f642a8a629d9f
Abbos Zoirov <122oszoirov@gmail.com>
2024-04-27 22:59:08 +0500

    make migrations

--- links/models.py
+++ links/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 class Link(models.Model):
     content = models.CharField(max_length=500)
+    title = models.CharField(max_length=150, default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
-        return self.content
+        return self.title
